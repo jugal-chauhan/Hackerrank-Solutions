048de0155aeebd66b3ad4452ca796e935bd13855
Jugal Deepak Chauhan <jugaldc2000@gmail.com>
2020-05-13 02:26:40 +0530

    initial commit

---
+++ Python/Introduction/WriteAFunction.py
@@ -0,0 +1,10 @@
+# @author: Jugal Chauhan
+
+def is_leap(year):
+    leap = False
+    if year%400==0 or ((year%4==0) and (year%100!=0)):
+        leap = True
+    # Write your logic here
+    return leap
+year = int(input())
+print(is_leap(year))
